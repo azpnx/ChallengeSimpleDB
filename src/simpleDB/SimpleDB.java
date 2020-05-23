f16d376b1545d03a9f870c557bf4b13ee592fdac
Gabriel Garcez <procurando.gabriel@hotmail.com>
2020-05-23 13:11:28 -0300

    The method databaseName was created.

--- src/simpleDB/SimpleDB.java
+++ src/simpleDB/SimpleDB.java
@@ -1,5 +1,20 @@
 package simpleDB;
 
+import java.io.*;
+
 public class SimpleDB {
+	String databaseName;
+
+	// Method to create a directory with the name passed in parameter.  
+	public void createDatabase(String databaseName) throws FileNotFoundException {
+		this.databaseName = databaseName;
+
+		// Check error, if completed perfectly it returns "OK"
+		try {
+			new File(databaseName).mkdir();
+			System.out.println("OK");
+		}catch(Exception e){	
+		}
+	}
 
 }
