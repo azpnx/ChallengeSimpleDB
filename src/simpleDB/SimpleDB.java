31debdbaa844e643822b46d8f51f727c193f1e98
Gabriel Garcez <procurando.gabriel@hotmail.com>
2020-05-23 13:30:30 -0300

    The method createFile was created.

--- src/simpleDB/SimpleDB.java
+++ src/simpleDB/SimpleDB.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class SimpleDB {
 	String databaseName;
+	String fileName;
 
 	// Method to create a directory with the name passed in parameter.  
 	public void createDatabase(String databaseName) throws FileNotFoundException {
@@ -15,6 +16,19 @@ public class SimpleDB {
 			System.out.println("OK");
 		}catch(Exception e){	
 		}
+	}
+
+	// Method that creates a file where the information will be read and saved.
+	public void createFile(String fileName) {
+		this.fileName = fileName;
+		File create = new File(databaseName, fileName+".txt");
+		try {
+			create.createNewFile();
+			System.out.print("Ok");
+		} catch (IOException e) {
+
+		}
+
 	}
 
 }
