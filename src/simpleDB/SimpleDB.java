0d28a869e615c6e1cffad2be6eb5a4c30d0e4425
Gabriel Garcez <procurando.gabriel@hotmail.com>
2020-07-07 13:19:01 -0300

    Improving the code

--- src/simpleDB/SimpleDB.java
+++ src/simpleDB/SimpleDB.java
@@ -6,89 +6,82 @@ import java.nio.file.*;
 import java.util.*;
 
 public class SimpleDB {
-	String databaseName;
-	String fileName;
-	int lineNumber;
-	String local;
-	String value;
+	private String databaseName;
+	private String fileName;
+	private Integer lineNumber;
+	private String path;
+	private String value;
 
-	// Method to create a directory with the name passed in parameter.  
+	public SimpleDB() {
+	}
+
+	public SimpleDB(String databaseName, String fileName, Integer lineNumber, String path, String value) {
+		this.databaseName = databaseName;
+		this.fileName = fileName;
+		this.lineNumber = lineNumber;
+		this.path = path;
+		this.value = value;
+	}
+
+	// Method to create a directory with the name passed in parameter.
 	public void createDatabase(String databaseName) throws FileNotFoundException {
 		this.databaseName = databaseName;
-
 		// Check error, if completed perfectly it returns "OK"
 		try {
 			new File(databaseName).mkdir();
 			System.out.println("simpledb> OK");
-		}catch(Exception e){	
+		} catch (Exception e) {
+			System.out.println("simpledb> Error:" + e.getMessage());
 		}
 	}
 
 	// Method that creates a file where the information will be read and saved.
 	public void createFile(String fileName) {
-		this.fileName = fileName;
-		File create = new File(databaseName, fileName+".txt");
+		File create = new File(databaseName + "/", fileName + ".txt");
 		try {
 			create.createNewFile();
 			System.out.print("simpledb> Ok");
 		} catch (IOException e) {
-
+			System.out.println("simpledb> Error:" + e.getMessage());
 		}
 
 	}
 
-	// Select the directory and file to be used 
-	public void use(String databaseName, String fileName)  {
-		this.databaseName = databaseName;
-		this.fileName = fileName;
-	
-		try {
-
-			local = databaseName +"/"+ fileName + ".txt";
-			System.out.println("simpledb> Selected <" + databaseName +">.<" +  fileName+">");
-		} catch (Exception e) {
-			System.out.print("simpledb> There is no such directory and file.");
-		}
-
+	// Select the directory and file to be used
+	public void use(String databaseName, String fileName) {
+		path = databaseName + "/" + fileName + ".txt";
+		System.out.println("simpledb> Selected <" + databaseName + ">.<" + fileName + ".txt>");
 	}
 
 	// Select the line to be read.
 	public void read(int lineNumber) throws IOException {
-		this.lineNumber = lineNumber;
-		String specific_line_text = Files.readAllLines(Paths.get(databaseName +"/"+ fileName+".txt")).get(lineNumber);
-		System.out.println("simpledb> "+specific_line_text);
+		String specific_line = Files.readAllLines(Paths.get(path)).get(lineNumber - 1);
+		System.out.println("simpledb> " + specific_line);
 
 	}
 
 	// Added the input to file
 	public void insert(String value) {
-		try
-		{
-			File f=new File(local);
-			PrintWriter insertFile=new PrintWriter(new FileOutputStream(f,true));
-			insertFile.append(value+"\n");
+		try {
+			File file = new File(path);
+			PrintWriter insertFile = new PrintWriter(new FileOutputStream(file, true));
+			insertFile.append(value + "\n");
 			insertFile.close();
 			System.out.println("Ok");
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			System.out.println("simpledb> There is no such directory and file.");
-		}  
+		}
 	}
 
 	// Removes a specific line and adds a value in place
 	public void update(int lineNumber, String value) throws IOException {
-		this.lineNumber = lineNumber;
-		this.value = value;
 		try {
-
-
-			Path path = Paths.get(databaseName +"/"+ fileName+".txt");
-			List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+			List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
 
 			System.out.println((lines.get(lineNumber - 1)).toString());
 			lines.remove(lineNumber - 1);
-			lines.add(lineNumber -1, value);
-			Files.write(path, lines, StandardCharsets.UTF_8);
+			lines.add(lineNumber - 1, value);
+			Files.write(Paths.get(path), lines, StandardCharsets.UTF_8);
 			System.out.println((lines.get(lineNumber - 1)).toString());
 			System.out.println(lines);
 
@@ -100,16 +93,55 @@ public class SimpleDB {
 
 	// Remove a specific line
 	public void delete(int lineNumber) throws IOException {
-		this.lineNumber = lineNumber;
 		try {
-			Path path = Paths.get(databaseName +"/"+ fileName+".txt");
-			List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+			List<String> lines = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
+			String deletedLine = Files.readAllLines(Paths.get(path)).get(lineNumber - 1);
+			System.out.println("simpledb> " + deletedLine);
 			lines.remove(lineNumber - 1);
-			Files.write(path, lines, StandardCharsets.UTF_8);}
-		catch(Exception e) {
+			Files.write(Paths.get(path), lines, StandardCharsets.UTF_8);
+		} catch (Exception e) {
 			System.out.print("simpledb> There is no such directory and file.");
 		}
 	}
-	
-	
+
+	public String getDatabaseName() {
+		return databaseName;
+	}
+
+	public void setDatabaseName(String databaseName) {
+		this.databaseName = databaseName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Integer getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(Integer lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
--- src/simpleDB/Teste.java
+++ src/simpleDB/Teste.java
@@ -1,26 +1,41 @@
 package simpleDB;
-import java.io.*;
-import java.util.*;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Teste {
 
-
 	public static void main(String[] args) throws IOException {
 		SimpleDB command1 = new SimpleDB();
-		
-		String a;
-		String b;
-		Scanner scanj = new Scanner(System.in);
-		System.out.println("databasename >");
-		a = scanj.nextLine();
-		System.out.println("filename >");
-		b = scanj.nextLine();
-		scanj.close();
-		
-		command1.use(""+a, "" +b);
-		
-		command1.update(3, "jacare222");
-		command1.delete(2);
+		String db = "simpledb> ";
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		 * System.out.print(db + "Database name: "); String databaseName =
+		 * sc.nextLine(); command1.createDatabase(databaseName);
+		 * 
+		 * System.out.println(db + "File name: "); String fileName = sc.nextLine();
+		 * command1.createFile(fileName);
+		 */
+
+		System.out.println(db + "File path: ");
+		System.out.print(db + "Database name: ");
+		String databaseName = sc.nextLine();
+		System.out.println(db + "File name: ");
+		String fileName = sc.nextLine();
+		command1.use(databaseName, fileName);
+
+		/*
+		 * System.out.println(db + "Enter a text: "); String value = sc.nextLine();
+		 * command1.insert(value);
+		 */
+		command1.read(2);
+
+		System.out.println("Enter a line to delete: ");
+		int lineNumber = sc.nextInt();
+		command1.delete(lineNumber);
+
+		sc.close();
 	}
 
 }
